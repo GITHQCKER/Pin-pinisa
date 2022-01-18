0e864ea3f81d2cc81c90e1dcc201ec45978c4573
IMASH NIDUSHA (I AM ZED) <96275141+GITHQCKER@users.noreply.github.com>
2022-01-18 17:44:04 +0530

    Initial commit

---
+++ plugins/donasi.js
@@ -0,0 +1,55 @@
+/**
+ * TOLONG JANGAN GANTI GAMBARNYA,NOMORKU DAN SAWERIAKU
+ * MENDING KALIAN TAMBAHIN NOMOR KALIAN
+*/
+
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+let handler = async (m) => {
+let duit = `*────── 「 BOT KINGDOM 」 ──────*
+
+
+HAI
+https://chat.whatsapp.com/CIDWlH7yzEsKombRDcTKcV`
+let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/1afe4bdf9c931fdb6f54d.jpg' }}, { upload: conn.waUploadToServer })
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           imageMessage: message.imageMessage,
+           hydratedContentText: duit,
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '🏧 MY WEBSITE',
+               url: 'supuna.ml'
+             }
+
+           },
+               {
+             callButton: {
+               displayText: 'call',
+               phoneNumber: '+94'
+             }
+           },           
+               {
+             quickReplyButton: {
+               displayText: '🧒 Owner',
+               id: '.owner',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)|bagiduit$/i
+
+module.exports = handler
